c5f607e9516af818682505df2d72e0d7601cfd8b
Mario Lisbona <mario.lisbona@gmail.com>
2022-08-24 15:00:21 +1000

    Start adding comments to script.js

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+//Variable for the scroll up button element
 const btnScrollUp = document.getElementById('btn-scrollup');
 
+// As soon as th window loads set the display to none.
+//This was needed as the button was showing when the window loaded
 window.onload = btnScrollUp.style.display = 'none';
 
+//call the showHideButton everytime the user srolls the window
 window.onscroll = function() {
-    ShowHideButton()
+    showHideButton()
 };
 
-function ShowHideButton() {
+//checks to see whether the page has been scrolled more than 30px from the top of the body or the document and shows the button if either is trure
+function showHideButton() {
     if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
         btnScrollUp.style.display = "block";
       } else {
@@ -14,6 +19,7 @@ function ShowHideButton() {
       }
 }
 
+//event listener on the scroll up button. When clicked it will set the scroll value to 0 on the body and document elements.
 btnScrollUp.addEventListener('click', ()=> {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
@@ -21,6 +27,7 @@ btnScrollUp.addEventListener('click', ()=> {
 
 // ///////////////////////IMG MODAL ////////////////////////
 // Open the Modal
+
 function openModal() {
   document.getElementById("myModal").style.display = "block";
 }
@@ -48,8 +55,12 @@ function showSlides(n) {
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("demo");
   var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1
+    }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
